71f0094a689bcaef35f8a41809879b4495330e69
anugrahdev <anangnugraha8@gmail.com>
2021-09-25 14:32:00 +0700

    Initial Commit

---
+++ FitNessTests/AppModelTests.swift
@@ -0,0 +1,35 @@
+//
+//  AppModelTests.swift
+//  FitNessTests
+//
+//  Created by Anang Nugraha on 25/09/21.
+//  Copyright © 2021 Razeware. All rights reserved.
+//
+
+import XCTest
+@testable import FitNess
+
+class AppModelTests: XCTestCase {
+  
+  var sut: AppModel!
+  
+  override func setUp() {
+    super.setUp()
+    sut = AppModel()
+  }
+
+  func testAppModel_whenInitialized_isInNotStartedState()  {
+    let initialState = sut.appState
+    XCTAssertEqual(initialState, AppState.notStarted)
+  }
+  
+  func testAppModel_whenStarted_isInInProgressState() {
+    //given
+    let expectedState = AppState.inProgress
+    
+    //when
+    sut.start()
+    
+    XCTAssertEqual(sut.appState, expectedState)
+  }
+}
