439d233fab882c4b47beb17a56fcd7d5326cf551
anugrahdev <anangnugraha8@gmail.com>
2021-09-26 15:13:50 +0700

    test fail check

--- FitNessTests/AppModelTests.swift
+++ FitNessTests/AppModelTests.swift
@@ -20,7 +20,7 @@ class AppModelTests: XCTestCase {
 
   func testAppModel_whenInitialized_isInNotStartedState()  {
     let initialState = sut.appState
-    XCTAssertEqual(initialState, AppState.notStarted)
+    XCTAssertEqual(initialState, AppState.completed)
   }
   
   func testAppModel_whenStarted_isInInProgressState() {
